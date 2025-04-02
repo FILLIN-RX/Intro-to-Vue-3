14a64f56e8abd8c0e0df4835764c12e4cfcdaffd
RUXEL <djeutchouruxel@gmail.com>
2025-04-02 19:38:00 +0100

    learning vue.js

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return{
+            product:"Socks",
+            description:"the best sock ever seen",
+            image:"assets/images/socks_green.jpg",
+            url:"https://chatgpt.com/c/67ed0170-5dac-800f-8b41-f17b8e3fbbb9",
+            Inventory:100,
+            onSale:false,
+            details:["50% cotton" ,"30% wool", "20% polyester"],
+            varients:[ 
+                { id:2234, color:"green" ,image:"assets/images/socks_green.jpg"},
+                {id:2235, color:"blue",image:"assets/images/socks_blue.jpg"}
+            ],
+            cart:0
+
+        }
+        
+        
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        removeToCart(){
+            if (this.cart>=1) {
+                this.cart -=1
+                
+            }
+            
+
+        },
+        updateImage(variantImage){
+            this.image=variantImage
+        }
+        
+    }
+})
