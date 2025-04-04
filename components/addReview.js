9f36cea4d46b0bbba00328e4af0534117b1886a8
RUXEL <djeutchouruxel@gmail.com>
2025-04-04 19:45:10 +0100

    main

--- components/addReview.js
+++ components/addReview.js
@@ -6,15 +6,15 @@ app.component('review-form',{
 
     /*html*/
     template:
-    `<form class="review-form">
+    `<form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name">
+        <input id="name" v-model="name">
         <label for="review">Review</label>
-        <textarea id="review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
 
         <label for="rating">Rating</label>
-        <select>
+        <select id="rating" v-model.number="rating">
             <option>5</option>
             <option>4</option>
             <option>3</option>
@@ -26,5 +26,27 @@ app.component('review-form',{
         <input class="button" type="submit" value="Submit">
     
     
-    </form>`
+    </form>`,
+    data() {
+        return {
+            name:'',
+            review:'',
+            rating:null
+        }
+    },
+    methods:{
+        onsubmit(){
+            let productReview={
+                name:this.name,
+                review:this.review,
+                rating:this.rating
+            }
+            this.$emit('review-submitted',productReview)
+
+            this.name='',
+            this.review='',
+            this.rating=''
+        }
+
+    }
 })
